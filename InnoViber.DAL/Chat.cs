975247abf3866a90f6f56697729806a727f76ae8
Leeyaoi <darya.ya2608@gmail.com>
2024-06-04 18:57:13 +0300

    deleted unnesessary files

--- InnoViber.DAL/Chat.cs
+++ InnoViber.DAL/Chat.cs
@@ -2,7 +2,7 @@
 {
     public class Chat
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = "";
         List<User> Users { get; set; } = new();
         List<Message> Messages { get; set;} = new();
