1a1c80787df19a1ef0571903f6e91b4da8548da8
Leeyaoi <darya.ya2608@gmail.com>
2024-06-04 19:02:17 +0300

    fixed according to comments

--- InnoViber.DAL/User.cs
+++ InnoViber.DAL/User.cs
@@ -1,11 +1,13 @@
-﻿namespace InnoViber.DAL
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace InnoViber.DAL
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Surname { get; set; } = "";
-        public string Email { get; set; } = "";
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public List<Message> Messages { get; set; } = new();
         public List<Chat> Chats { get; set; } = new();
     }
