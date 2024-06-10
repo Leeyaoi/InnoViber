94a0aff2d9715d477ac929c171042be222a6b2cd
Leeyaoi <darya.ya2608@gmail.com>
2024-06-10 16:10:34 +0300

    fixed enum

--- InnoViber.DAL/Entities/MessageEntity.cs
+++ InnoViber.DAL/Entities/MessageEntity.cs
@@ -4,9 +4,16 @@ public class MessageEntity : BaseEntity
 {
     public DateTime Date { get; set; }
     public string Text { get; set; } = string.Empty;
-    public enum Status;
+    public MessageStatus Status {  get; set; }
     public Guid UserId{ get; set; }
     public Guid ChatId{ get; set; }
     public UserEntity? User { get; set; }
     public ChatEntity? Chat { get; set; }
+
+    public enum MessageStatus
+    {
+        Send = 0,
+        Delivered = 1,
+        Read = 2
+    }
 }
--- InnoViber/View models/MessageViewModel.cs
+++ InnoViber/View models/MessageViewModel.cs
@@ -6,5 +6,12 @@ public class MessageViewModel : BaseViewModel
 {
     public DateTime Date { get; set; }
     public string Text { get; set; } = string.Empty;
-    public enum Status;
+    public MessageStatus Status {  get; set; }
+
+    public enum MessageStatus
+    {
+        Send = 0,
+        Delivered = 1,
+        Read = 2
+    }
 }
