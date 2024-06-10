d088f728d1efb4fbc4313c71bb96df0b3f7b36d1
Leeyaoi <116959359+Leeyaoi@users.noreply.github.com>
2024-06-10 15:45:05 +0300

    added view models and m<->vm mapping (#10)

---
+++ InnoViber/View models/BaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InnoViber.BLL.Models;
+
+public class BaseViewModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ InnoViber/View models/ChatViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace InnoViber.BLL.Models;
+
+public class ChatViewModel : BaseViewModel
+{
+    public string Name { get; set; } = string.Empty;
+}
