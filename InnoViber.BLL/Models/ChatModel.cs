0f33ef1ba961b11fa0adead0211dcbbfe5f84067
Leeyaoi <darya.ya2608@gmail.com>
2024-06-13 18:27:10 +0300

    added chat owner

--- InnoViber.BLL/Models/ChatModel.cs
+++ InnoViber.BLL/Models/ChatModel.cs
@@ -3,6 +3,8 @@
 public class ChatModel : BaseModel
 {
     public string Name { get; set; } = string.Empty;
+    public Guid OwnerId { get; set; }
+    public UserModel Owner { get; set; } = new();
     List<UserModel> Users { get; set; } = new();
     List<MessageModel> Messages { get; set; } = new();
 }
