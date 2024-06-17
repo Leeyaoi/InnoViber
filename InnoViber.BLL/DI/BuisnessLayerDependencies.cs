5aaccd9e69a79b8abab645aebd1fc2fb35ee516a
Leeyaoi <116959359+Leeyaoi@users.noreply.github.com>
2024-06-17 15:29:14 +0300

    Feature/added backgroung worker (#37)
    * added backgroung worker
    
    * deleted unnesessary uses

--- InnoViber.BLL/DI/BuisnessLayerDependencies.cs
+++ InnoViber.BLL/DI/BuisnessLayerDependencies.cs
@@ -1,13 +1,6 @@
-using AutoMapper.Extensions.ExpressionMapping;
-using InnoViber.BLL.Helpers;
 using InnoViber.BLL.Interfaces;
-using InnoViber.BLL.Models;
 using InnoViber.BLL.Services;
-using InnoViber.DAL.Interfaces;
-using InnoViber.DAL.Models;
-using InnoViber.DAL.Repositories;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.CompilerServices;
 
 namespace InnoViber.BLL.DI;
 
@@ -15,6 +8,7 @@ public static class BuisnessLayerDependencies
 {
     public static void RegisterBLLDependencies(this IServiceCollection services)
     {
+        services.AddSingleton<CheckIsSeenMessagesService>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IChatService, ChatService>();
         services.AddScoped<IMessageService, MessageService>();
---
+++ InnoViber.BLL/Services/CheckIsSeenMessagesService.cs
@@ -0,0 +1,36 @@
+﻿using InnoViber.BLL.Interfaces;
+using InnoViber.Domain.Providers;
+using Microsoft.Extensions.Hosting;
+
+namespace InnoViber.BLL.Services;
+
+public class CheckIsSeenMessagesService : BackgroundService
+{
+    private readonly TimeSpan _period;
+    private readonly IMessageService _messageService;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public CheckIsSeenMessagesService(IMessageService messageService, IDateTimeProvider timeProvider)
+    {
+        _period = TimeSpan.FromMinutes(20);
+        _messageService = messageService;
+        _dateTimeProvider = timeProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new PeriodicTimer(_period);
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync())
+        {
+            var messages = await _messageService.GetAll(stoppingToken);
+            foreach (var message in messages)
+            {
+                var howLong = (message.Date - _dateTimeProvider.GetDate()).TotalMinutes;
+                if(!message.IsSeen() && howLong > 30)
+                {
+                    continue; //this is for now
+                }
+            }
+        }
+    }
+}
