db2eac7f8d6987e418a17b74aa7c9951b7ef01e0
Leeyaoi <darya.ya2608@gmail.com>
2024-06-19 14:55:56 +0300

    added validaion

---
+++ InnoViber/Validators/ChatRoleViewModelValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using InnoViber.API.ViewModels.ChatRole;
+
+namespace InnoViber.API.Validators;
+
+public class ChatRoleViewModelValidation : AbstractValidator<ChatRoleViewModel>
+{
+    public ChatRoleViewModelValidation()
+    {
+        RuleFor(x => x.Role).NotNull();
+        RuleFor(x => x.ChatId).NotEmpty();
+        RuleFor(x => x.UserId).NotEmpty();
+    }
+}
