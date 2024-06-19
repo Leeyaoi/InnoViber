22907d44edbb7b9538b0565fcccdfa42dac6fc82
Leeyaoi <darya.ya2608@gmail.com>
2024-06-19 14:38:10 +0300

    added service

---
+++ InnoViber.BLL/Interfaces/IChatRoleService.cs
@@ -0,0 +1,9 @@
+﻿using InnoViber.BLL.Models;
+using InnoViber.Domain.Enums;
+
+namespace InnoViber.BLL.Interfaces;
+
+public interface IChatRoleService : IGenericService<ChatRoleModel>
+{
+    Task<ChatRoleModel> UpdateRole(UserRoles role, ChatRoleModel model, CancellationToken ct);
+}
