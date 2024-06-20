58c147d6e67e43db12d90c3248064b1e9805cbd6
Leeyaoi <darya.ya2608@gmail.com>
2024-06-20 17:40:10 +0300

    added fluent validation tests

---
+++ InnoViber.Test.Validation/MessageChangeStatusViewModelValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using InnoViber.API.Validators;
+using InnoViber.API.ViewModels.Message;
+using InnoViber.API.ViewModels.User;
+
+namespace InnoViber.Test.Validation;
+
+public class MessageChangeStatusViewModelValidationTests
+{
+    private readonly MessageChangeStatusViewModelValidation _validator;
+
+    public MessageChangeStatusViewModelValidationTests()
+    {
+        _validator = new MessageChangeStatusViewModelValidation();
+    }
+
+    [Fact]
+    public void MessageStatusValidator_ShouldPass_WhenEverythingIsValid()
+    {
+        // Arrange
+        var user = new MessageChangeStatusViewModel()
+        {
+            Status = 0
+        };
+
+        // Act
+        var result = _validator.Validate(user);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
---
+++ InnoViber.Test.Validation/MessageViewModelValidationTests.cs
@@ -0,0 +1,115 @@
+﻿using InnoViber.API.Validators;
+using InnoViber.API.ViewModels.Message;
+using InnoViber.API.ViewModels.User;
+
+namespace InnoViber.Test.Validation;
+
+public class MessageViewModelValidationTests
+{
+    private readonly MessageViewModelValidation _validator;
+
+    public MessageViewModelValidationTests()
+    {
+        _validator = new MessageViewModelValidation();
+    }
+
+    [Fact]
+    public void MessageValidator_ShouldPass_WhenEverythingIsValid()
+    {
+        // Arrange
+        var message = new MessageShortViewModel()
+        {
+            Date = DateTime.Now,
+            Text = "Test",
+            Status = 0,
+            UserId = Guid.NewGuid(),
+            ChatId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = _validator.Validate(message);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void MessageValidator_ShouldFail_WhenDateIsEmpty()
+    {
+        // Arrange
+        var message = new MessageShortViewModel()
+        {
+            Date = new(),
+            Text = "Test",
+            Status = 0,
+            UserId = Guid.NewGuid(),
+            ChatId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = _validator.Validate(message);
+
+        // Assert
+        Assert.False(result.IsValid);
+    }
+
+    [Fact]
+    public void MessageValidator_ShouldFail_WhenTextIsEmpty()
+    {
+        // Arrange
+        var message = new MessageShortViewModel()
+        {
+            Date = DateTime.Now,
+            Text = "",
+            Status = 0,
+            UserId = Guid.NewGuid(),
+            ChatId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = _validator.Validate(message);
+
+        // Assert
+        Assert.False(result.IsValid);
+    }
+
+    [Fact]
+    public void MessageValidator_ShouldFail_WhenUserIdIsEmpty()
+    {
+        // Arrange
+        var message = new MessageShortViewModel()
+        {
+            Date = DateTime.Now,
+            Text = "Test",
+            Status = 0,
+            UserId = new(),
+            ChatId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = _validator.Validate(message);
+
+        // Assert
+        Assert.False(result.IsValid);
+    }
+
+    [Fact]
+    public void MessageValidator_ShouldFail_WhenChatIdIsEmpty()
+    {
+        // Arrange
+        var message = new MessageShortViewModel()
+        {
+            Date = DateTime.Now,
+            Text = "Test",
+            Status = 0,
+            UserId = Guid.NewGuid(),
+            ChatId = new()
+        };
+
+        // Act
+        var result = _validator.Validate(message);
+
+        // Assert
+        Assert.False(result.IsValid);
+    }
+}
