b20625f0b2d4010eebc120f11c58250779f7a14f
Leeyaoi <darya.ya2608@gmail.com>
2024-06-20 16:56:50 +0300

    mock masstransit

--- InnoViber.Test.Integration/DataBaseWebApplicationFactory.cs
+++ InnoViber.Test.Integration/DataBaseWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using InnoViber.DAL.Data;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,16 +16,20 @@ public class DataBaseWebApplicationFactory<TProgram>
         WebHost = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
             builder.ConfigureServices(services =>
             {
-                var dbContextDescriptor =
-                    services.SingleOrDefault(i =>
-                        i.ServiceType == typeof(DbContextOptions<ViberContext>));
+                var dbContextDescriptor = services.SingleOrDefault(i =>i.ServiceType == typeof(DbContextOptions<ViberContext>));
 
                 services.Remove(dbContextDescriptor!);
+
+                var publishEndpoint = services.SingleOrDefault(i => i.ServiceType == typeof(IPublishEndpoint));
+
+                services.Remove(publishEndpoint!);
 
                 services.AddDbContext<ViberContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryDbContextTest");
                 });
+
+                services.AddMassTransitTestHarness();
             }));
     }
 }
