fa65a1f5691df85c895e2e8405c519ccf08a3e83
Leeyaoi <116959359+Leeyaoi@users.noreply.github.com>
2024-06-24 15:34:00 +0300

    feature/add-user-service-with-mongodb (#52)
    * Add files via upload
    
    * Update DataLayerDepencies.cs

---
+++ InnoViberUserService/UserService.DAL/DI/DataLayerDepencies.cs
@@ -0,0 +1,14 @@
+﻿using UserService.DAL.Data;
+using Microsoft.Extensions.DependencyInjection;
+using UserService.DAL.Interfaces;
+using UserService.DAL.Repositories;
+
+namespace UserService.DAL.DI;
+
+public static class DataLayerDepencies
+{
+    public static void RegisterDALDependencies(this IServiceCollection services)
+    {
+        services.AddSingleton<MongoDbContext>();
+    }
+}
---
+++ InnoViberUserService/UserService.DAL/Data/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace UserService.DAL.Data;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DbConnection");
+        var mongoUrl = new MongoUrl(connectionString);
+        var mongoClient = new MongoClient(mongoUrl);
+        _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+    }
+
+    public IMongoDatabase Database => _database;
+}
