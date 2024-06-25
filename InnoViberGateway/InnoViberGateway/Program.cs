f40ddb33a1ff805002752372da426b3ad3686c9a
Leeyaoi <darya.ya2608@gmail.com>
2024-06-25 10:55:48 +0300

    added fully working ocelot

--- InnoViberGateway/InnoViberGateway/Program.cs
+++ InnoViberGateway/InnoViberGateway/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
+namespace InnoViberGateway;
 
-app.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+        builder.Services.AddOcelot(builder.Configuration);
+
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapControllers();
+        app.UseOcelot().GetAwaiter();
+
+        app.Run();
+
+    }
+}
+
+
