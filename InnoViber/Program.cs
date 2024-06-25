4da06d7820ff329079ad3070091a529dd42bfedc
Leeyaoi <116959359+Leeyaoi@users.noreply.github.com>
2024-06-25 14:06:17 +0300

    Feature/add ocelot gateway (#57)
    * Add files via upload
    
    * added user repository
    
    * added user service
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * added controller
    
    * slightly fixed chatRole controller
    
    * create ocelot solution
    
    * added fully working ocelot

--- InnoViber/Program.cs
+++ InnoViber/Program.cs
@@ -4,57 +4,56 @@ using InnoViber.API.DI;
 using InnoViber.Domain.DI;
 using InnoViber.API.Extensions;
 
-namespace InnoViber
+namespace InnoViber;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.RegisterDALDependencies(builder.Configuration);
+
+        builder.Services.RegisterBLLDependencies();
+
+        builder.RegisterAPIDependencies();
+
+        builder.Services.RegisterDomainDependencies();
+
+        builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(BuisnessLayerDependencies).Assembly);
+
+        var app = builder.Build();
+
+        app.UseExeptionHandlerMiddleware();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddEndpointsApiExplorer();
-
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.RegisterDALDependencies(builder.Configuration);
-
-            builder.Services.RegisterBLLDependencies();
-
-            builder.RegisterAPIDependencies();
-
-            builder.Services.RegisterDomainDependencies();
-
-            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(BuisnessLayerDependencies).Assembly);
-
-            var app = builder.Build();
-
-            app.UseExeptionHandlerMiddleware();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(options => 
             {
-                app.UseSwagger();
-                app.UseSwaggerUI(options => 
-                {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                    options.RoutePrefix = string.Empty;
-                });
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
---
+++ InnoViberGateway/InnoViberGateway/Program.cs
@@ -0,0 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace InnoViberGateway;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+        builder.Services.AddOcelot(builder.Configuration);
+
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapControllers();
+        app.UseOcelot().GetAwaiter();
+
+        app.Run();
+
+    }
+}
+
+
