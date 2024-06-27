ef1b582794061216d8b416bc637ca1e072ffa7e6
Leeyaoi <116959359+Leeyaoi@users.noreply.github.com>
2024-06-27 14:31:46 +0300

    Feature/add new user dal (#66)
    * fixed models and entities
    
    * updated tests
    
    * fixed validation
    
    * fixed user controller
    
    * added new user dal
    
    * fixed bg worker
    
    * fixed di

---
+++ InnoViber.User.DAL/Models/ExternalUserModel.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace InnoViber.User.DAL.Models;
+
+public class ExternalUserModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string About { get; set; } = string.Empty;
+    public BsonBinaryData? UserPhoto { get; set; }
+}
