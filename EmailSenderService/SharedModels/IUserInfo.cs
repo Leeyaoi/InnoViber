d224c9e469c54a73d9d0b61f1fed6336e03247f3
Leeyaoi <darya.ya2608@gmail.com>
2024-08-13 18:43:56 +0300

    added dockerffile and docker compose

---
+++ EmailSenderService/SharedModels/IUserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedModels;
+
+public interface IUserInfo
+{
+    string UserName { get; }
+    string Email { get; }
+    double HowLong { get; }
+    string AuthorName { get; }
+    string ChatName { get; }
+}
